2eb80118d2039da91a87deb73f49250fee1ae886
Nikola Tosic <55281244+NikolaTosic-sudo@users.noreply.github.com>
2020-11-11 13:19:37 +0100

    Migrations
    Made migrations to start the app

--- partyControl/partyControl/settings.py
+++ partyControl/partyControl/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'spotify_api.apps.SpotifyApiConfig',
+    'sportify_api.apps.SportifyApiConfig',
     'rest_framework'
 ]
 
--- partyControl/partyControl/urls.py
+++ partyControl/partyControl/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('spotify_api.urls'))
+    path('', include('sportify_api.urls'))
 ]
