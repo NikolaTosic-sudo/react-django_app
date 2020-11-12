0f0f4db88c11b18dc91d78f56e0c9a64bab8f52c
Nikola Tosic <55281244+NikolaTosic-sudo@users.noreply.github.com>
2020-11-12 13:11:17 +0100

    Update models.py
    Option to vote for skiping a song, 1 vote by default

--- partyControl/sportify_api/models.py
+++ partyControl/sportify_api/models.py
@@ -6,3 +6,4 @@ class Room(models.Model):
     code = models.CharField(max_lenght=8, default="", unique=True)
     host = models.CharField(max_length=50, unique=True)
     guest_can_pause = models.BooleanField(null=False, default=False)
+    votes_to_skip = models.IntegerField(null=False, default=1)
