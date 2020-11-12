afa9b7aab82f35778d5464b67420d2c4cf5daae8
Nikola Tosic <55281244+NikolaTosic-sudo@users.noreply.github.com>
2020-11-12 13:37:28 +0100

    Update views.py
    Created ApiView with RoomSerializer

--- partyControl/sportify_api/views.py
+++ partyControl/sportify_api/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rest_framework import generics
+from .serializers import RoomSerializer
+from .models import Room
 
 # Create your views here.
+
+class RooView(generics.CreateAPIView):
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
 
 
 def main(request):
