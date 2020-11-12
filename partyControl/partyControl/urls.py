5b3e8fcdd38429ad56e940518ccb552553941c12
Nikola Tosic <55281244+NikolaTosic-sudo@users.noreply.github.com>
2020-11-12 13:59:58 +0100

    Test
    Testing my code and adding one RoomModel

--- partyControl/partyControl/urls.py
+++ partyControl/partyControl/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('sportify_api.urls'))
+    path('api', include('sportify_api.urls'))
 ]
--- partyControl/sportify_api/views.py
+++ partyControl/sportify_api/views.py
@@ -1,15 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from rest_framework import generics
 from .serializers import RoomSerializer
 from .models import Room
 
 # Create your views here.
 
-class RooView(generics.CreateAPIView):
+class RoomView(generics.CreateAPIView):
     queryset = Room.objects.all()
     serializer_class = RoomSerializer
-
-
-def main(request):
-    return HttpResponse("<h1 style='color: red;'>Hello</h1>")
