27b18d4af7321093c06c6487c45ddeb05ca56113
Nikola Tosic <55281244+NikolaTosic-sudo@users.noreply.github.com>
2020-11-23 12:38:33 +0100

    Create CreateRoomPage.js
    For creating Party Rooms

---
+++ partyControl/frontend/src/components/CreateRoomPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class CreateRoomPage extends Component {
+    constructor(props){
+        super(props)
+    }
+
+    render(){
+        return <p>Create Room Page</p>
+    }
+}
+
+export default CreateRoomPage
