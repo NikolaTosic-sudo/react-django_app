d0363a477fd8e69ee08e48d2c2ccbcb037c67d84
Nikola Tosic <55281244+NikolaTosic-sudo@users.noreply.github.com>
2020-11-23 12:38:48 +0100

    Update App.js
    Adding HomePage to app for render

--- partyControl/frontend/src/components/App.js
+++ partyControl/frontend/src/components/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import HomePage from './HomePage'
 
 class App extends Component {
     constructor(props){
@@ -6,7 +7,12 @@ class App extends Component {
     }
 
     render(){
-        return <h1>TESTING REACT CODE</h1>
+        return (
+            <div>
+                <h1>TESTING REACT CODE</h1>
+                <HomePage />
+            </div>
+        )
     }
 }
 
