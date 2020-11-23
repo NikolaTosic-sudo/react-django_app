7bd6685e01acd02f5f48178c2185a2739489c570
Nikola Tosic <55281244+NikolaTosic-sudo@users.noreply.github.com>
2020-11-23 12:38:21 +0100

    Create HomePage.js
    This will be HomePage and will render everything, using Router and different Routes

---
+++ partyControl/frontend/src/components/HomePage.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Switch, Route, Link, Redirect } from 'react-router-dom'
+
+import CreateRoomPage from "./CreateRoomPage"
+import JoinRoomPage from "./JoinRoomPage"
+
+class HomePage extends Component {
+    constructor(props){
+        super(props)
+    }
+
+    render(){
+        return (
+            <Router>
+                <Switch>
+                    <Route exact path="/"><p>Home Page</p></Route>
+                    <Route path="/create" component={CreateRoomPage} />
+                    <Route path="/join" component={JoinRoomPage} />
+                </Switch>
+            </Router>
+        )
+    }
+}
+
+export default HomePage
