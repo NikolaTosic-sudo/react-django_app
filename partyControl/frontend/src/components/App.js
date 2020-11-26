0390270795e165199ee204870e56d77266ffa14f
Nikola Tosic <55281244+NikolaTosic-sudo@users.noreply.github.com>
2020-11-26 12:18:16 +0100

    Rendering problem
    Run into a problem, react part of the project does not render properly, django shows only old pages

--- partyControl/frontend/src/components/App.js
+++ partyControl/frontend/src/components/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
 import HomePage from './HomePage'
 
-class App extends Component {
+export default class App extends Component {
     constructor(props){
         super(props)
     }
@@ -9,11 +10,13 @@ class App extends Component {
     render(){
         return (
             <div>
-                <h1>TESTING REACT CODE</h1>
+                <h1>TESTING CODE</h1>
                 <HomePage />
             </div>
         )
     }
 }
 
-export default App
+const appDiv = document.getElementById('app');
+
+ReactDOM.render(<App />, appDiv);
