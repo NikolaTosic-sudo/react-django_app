87011947db6f8f0e97f94aa1e43282964cad63f7
Nikola Tosic <55281244+NikolaTosic-sudo@users.noreply.github.com>
2020-11-30 15:13:46 +0100

    Update Room.js
    Adding logic to the room, to fetch data from back-end

--- partyControl/frontend/src/components/Room.js
+++ partyControl/frontend/src/components/Room.js
@@ -11,6 +11,22 @@ class Room extends Component {
             guestCanPause: false,
             isHost: false
         }
+        this.roomCode = this.props.match.param.roomCode
+        this.getRoomDetails()
+    }
+
+
+    getRoomDetails = () => {
+
+        fetch('/api/get-room' + '?code=' + this.roomCode)
+            .then(response => response.json)
+            .then(data => {
+                this.setState({
+                    votesToSkip: data.votes_to_skip,
+                    guestCanPause: data.guest_can_pause,
+                    isHost: data.is_host
+                })
+            })
     }
 
 
@@ -19,6 +35,7 @@ class Room extends Component {
 
         return (
             <div>
+                <h3>{this.roomCode}</h3>
                 <p>{this.state.votesToSkip}</p>
                 <p>{this.state.guestCanPause.toString()}</p>
                 <p>{this.state.isHost.toString()}</p>
