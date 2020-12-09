69626884f4f8962fac73c67c715d2ff64dc10918
Nikola Tosic <55281244+NikolaTosic-sudo@users.noreply.github.com>
2020-12-09 14:23:54 +0100

    Update views.py
    Created new View to return code of the room if user is already in the room

--- partyControl/sportify_api/views.py
+++ partyControl/sportify_api/views.py
@@ -4,6 +4,7 @@ from .serializers import RoomSerializer, CreateRoomSerializer
 from .models import Room
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from django.http import JsonResponse
 
 
 # Create your views here.
@@ -86,3 +87,16 @@ class CreateRoomView(APIView):
 
 
             return Response(RoomSerializer(room).data, status=status.HTTP_201_CREATED)
+
+
+class UserInRoom(APIView):
+    def get(self, request, format=None):
+
+        if not self.request.session.exists(self.request.session.session_key):
+            self.request.session.create()
+
+        data = {
+            'code': self.request.session.get('room_code')
+        }
+
+        return JsonResponse(data, status=status.HTTP_200_OK)
