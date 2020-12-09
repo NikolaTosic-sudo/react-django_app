f98e9e25eb85e3434b507b7cb5bdf4f122f8b636
Nikola Tosic <55281244+NikolaTosic-sudo@users.noreply.github.com>
2020-12-09 13:22:21 +0100

    Update HomePage.js
    Making HomePage now, made a function there so code wouln't be bundled together and unreadable

--- partyControl/frontend/src/components/HomePage.js
+++ partyControl/frontend/src/components/HomePage.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { BrowserRouter as Router, Switch, Route, Link, Redirect } from 'react-router-dom'
+import { Grid, Button, ButtonGroup, Typography } from '@material-ui/core'
 
 import CreateRoomPage from "./CreateRoomPage"
 import JoinRoomPage from "./JoinRoomPage"
@@ -10,11 +11,30 @@ class HomePage extends Component {
         super(props)
     }
 
+
+    renderHomePage = () => {
+        return (
+            <Grid container spacing={3}>
+                <Grid item xs={12} align='center'>
+                    <Typography variant="h3" component="h3">
+                        House Party
+                    </Typography>
+                </Grid>
+                <Grid item xs={12} align='center'>
+                    <ButtonGroup disableElevation variant="contained" color='primary'>
+                        <Button color='primary' to='/join' component={Link}>Join a Room</Button>
+                        <Button color='secondary' to='/create' component={Link}>Create a Room</Button>
+                    </ButtonGroup>
+                </Grid>
+            </Grid>
+        )
+    }
+
     render(){
         return (
             <Router>
                 <Switch>
-                    <Route exact path="/"><p>Home Page</p></Route>
+                    <Route exact path="/">{this.renderHomePage}</Route>
                     <Route path="/create/" component={CreateRoomPage} />
                     <Route path="/join/" component={JoinRoomPage} />
                     <Route path="/room/:roomCode" component={Room} />
