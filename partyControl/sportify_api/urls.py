b25180a079eb8436c212f2d9b8c5d7d40218bc9b
Nikola Tosic <55281244+NikolaTosic-sudo@users.noreply.github.com>
2020-12-10 14:05:18 +0100

    Update urls.py
    New path for new view

--- partyControl/sportify_api/urls.py
+++ partyControl/sportify_api/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import RoomView, CreateRoomView, GetRoom, JoinRoomView, UserInRoom
+from .views import RoomView, CreateRoomView, GetRoom, JoinRoomView, UserInRoom, LeaveRoom
 
 urlpatterns = [
     path('', RoomView.as_view()),
     path('create-room', CreateRoomView.as_view()),
     path('get-room', GetRoom.as_view()),
     path('join-room', JoinRoomView.as_view()),
-    path('user-in-room', UserInRoom.as_view())
+    path('user-in-room', UserInRoom.as_view()),
+    path('leave-room', LeaveRoom.as_view())
 ]
